68f850f553d8c69542217cd0294bd78451ac77b9
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-10-20 12:12:30 +0200

    :heavy_plus_sign: Normalize.css

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 /**
  * Base styles
  */
+import 'normalize.css';
 import './styles/body.js'
 import './styles/heading.js'
 import './styles/paragraph.js'
