4485f16fd5df685d24363170617c86facd4ef8a5
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-10-20 09:39:30 +0200

    :sparkles: Logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="header">
+          <span className="logo">Eggg<span className="text-light">.io</span></span>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
